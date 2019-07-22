4e208d786d9bc51b4bffe6cc3d2df66244168b92
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-07-22 09:51:56 -0400

    cleaning up directory

---
+++ build/precache-manifest.a5b25c76d59afff19c124433fce3aaa0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6352bab20ac26f4bcb6d72b0dbb67a02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9e1c5c9fe8d54fc4bbd",
+    "url": "/static/css/main.ade27976.chunk.css"
+  },
+  {
+    "revision": "b525a7980ec4edc03655",
+    "url": "/static/js/2.8b998c98.chunk.js"
+  },
+  {
+    "revision": "d9e1c5c9fe8d54fc4bbd",
+    "url": "/static/js/main.a5161698.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
