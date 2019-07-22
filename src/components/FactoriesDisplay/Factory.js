95e939db49cd561b7ac51faa152d4dc992a8f54d
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-07-22 18:37:48 -0400

    Testing get request chains

--- src/components/FactoriesDisplay/Factory.js
+++ src/components/FactoriesDisplay/Factory.js
@@ -1,61 +1,82 @@
-import React, { useState } from 'react'
-import axios from 'axios'
+import React, { useState } from "react";
+import axios from "axios";
 
-import RenameForm from '../renameForm/renameForm.js';
+import RenameForm from "../renameForm/renameForm.js";
 
-import styles from './Factory.module.scss';
+import styles from "./Factory.module.scss";
 
 const Factory = ({ factory, root, setRoot }) => {
-  const [showForm, setShowForm] = useState(false)
-  console.log("Factory: ", factory)
+  const [showForm, setShowForm] = useState(false);
+  console.log("Factory: ", factory);
 
   const removeFactory = (e, id) => {
     e.preventDefault();
-    let keptFacts = []
-      root.filter(node => {
-        //console.log("node: ", node)
-        if (node._id !== factory._id) {
-          keptFacts.push(node)
-          //console.log("keptFacts: ", keptFacts)
-        }
-        //return keptFacts
-        setRoot(keptFacts)
-        console.log('factoryID: ', factory._id)
-        axiosRemove(factory._id)
-        return null
-      })
-  }
+    let keptFacts = [];
+    root.filter(node => {
+      //console.log("node: ", node)
+      if (node._id !== factory._id) {
+        keptFacts.push(node);
+        //console.log("keptFacts: ", keptFacts)
+      }
+      //return keptFacts
+      setRoot(keptFacts);
+      console.log("factoryID: ", factory._id);
+      axiosRemove(factory._id);
+      return null;
+    });
+  };
 
-  const axiosRemove = (id) => {
-    console.log("axiosRemove id: ", id)
-    const data = {_id: id}
+  const axiosRemove = id => {
+    console.log("axiosRemove id: ", id);
+    const data = { _id: id };
     //axios.delete('http://localhost:4000/remove-factory', {data}).then(res => {
-    axios.delete('https://full-stack-web-challenge.herokuapp.com/remove-factory', {data}).then(res => {
-      console.log(`res: ${res.data}`)
-    }).catch(err => {
-      console.log(err)
-    })
-  }
+    axios
+      .delete("https://full-stack-web-challenge.herokuapp.com/remove-factory", {
+        data
+      })
+      .then(res => {
+        console.log(`res: ${res.data}`);
+        axios
+          .get("https://full-stack-web-challenge.herokuapp.com/get-factory")
+          .then(res => console.log("axios remove: ", res.data));
+        setRoot(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
   //useEffect(() => {
-    //axiosRemove(id)
+  //axiosRemove(id)
   //},[removeFactory])
 
   return (
     <div className={styles.Wrapper}>
-    <div className={styles.Display} >
-    <p className={styles.Name}><span onClick={(e) => removeFactory(e, factory._id)}>X</span>&nbsp; Factory Name: &nbsp; {factory.factName}<span onClick={() => setShowForm(!showForm)}>^</span></p>
-    {showForm && <RenameForm factory={factory} root={root} setRoot={setRoot} showForm={showForm} setShowForm={setShowForm}  />}
-    {factory.children.map((child, index) => {
-      return (
-        <div key={index} className={styles.flex}>
-        <p className={styles.Children}>{child}</p>
-        </div>
-      )
-    })}
+      <div className={styles.Display}>
+        <p className={styles.Name}>
+          <span onClick={e => removeFactory(e, factory._id)}>X</span>&nbsp;
+          Factory Name: &nbsp; {factory.factName}
+          <span onClick={() => setShowForm(!showForm)}>^</span>
+        </p>
+        {showForm && (
+          <RenameForm
+            factory={factory}
+            root={root}
+            setRoot={setRoot}
+            showForm={showForm}
+            setShowForm={setShowForm}
+          />
+        )}
+        {factory.children.map((child, index) => {
+          return (
+            <div key={index} className={styles.flex}>
+              <p className={styles.Children}>{child}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
 export default Factory;
--- src/components/renameForm/renameForm.js
+++ src/components/renameForm/renameForm.js
@@ -1,11 +1,11 @@
-import React, {  useEffect } from 'react'
+import React, { useEffect } from "react";
 
-import axios from 'axios'
+import axios from "axios";
 
-import useFormState from '../../hooks/useFormState.js';
+import useFormState from "../../hooks/useFormState.js";
 
 const RenameForm = ({ factory, root, setRoot, showForm, setShowForm }) => {
-  const [updateName, setUpdateName] = useFormState("")
+  const [updateName, setUpdateName] = useFormState("");
 
   const submitNewName = (e, id) => {
     e.preventDefault();
@@ -14,48 +14,60 @@ const RenameForm = ({ factory, root, setRoot, showForm, setShowForm }) => {
     root.map(node => {
       //console.log(node.factName)
       if (node._id === id) {
-        update = updateName
-        console.log(update)
-        node.factName = update
-        console.log("updated root?: ", root)
+        update = updateName;
+        console.log(update);
+        node.factName = update;
+        console.log("updated root?: ", root);
       }
       //setRoot(root)
-      axiosUpdate(id, update)
-      setShowForm(!showForm)
-      return update
-    })
-    }
-  console.log(root)
+      axiosUpdate(id, update);
+      setShowForm(!showForm);
+      return update;
+    });
+  };
+  console.log(root);
   useEffect(() => {
-    setRoot(root)
-  }, [submitNewName])
+    setRoot(root);
+  }, [submitNewName]);
 
- const axiosUpdate = (id, name) => {
-   const data = {
-    _id: id,
-     newName: updateName
-   }
-   console.log("update data: ", data)
-   //axios.put('http://localhost:4000/update-factory', data).then(res => {
-   axios.put('https://full-stack-web-challenge.herokuapp.com/update-factory', data).then(res => {
-     console.log(`res, ${res.data}`)
-   }).catch(err => {
-     console.log(err)
-   })
-  }
+  const axiosUpdate = (id, name) => {
+    const data = {
+      _id: id,
+      newName: updateName
+    };
+    console.log("update data: ", data);
+    //axios.put('http://localhost:4000/update-factory', data).then(res => {
+    axios
+      .put(
+        "https://full-stack-web-challenge.herokuapp.com/update-factory",
+        data
+      )
+      .then(res => {
+        console.log(`res, ${res.data}`);
+        axios
+          .get("https://full-stack-web-challenge.herokuapp.com/get-factory")
+          .then(res => {
+            console.log("res update: ", res);
+            setRoot(res.data);
+          });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
   return (
     <form>
-    <input
-    type="text"
-    placeholder={factory.factName}
-    name="update"
-    value={updateName}
-    onChange={setUpdateName}
-    />
-    <button onClick={(e) => submitNewName(e, factory._id)}>Update</button>
+      <input
+        type="text"
+        placeholder={factory.factName}
+        name="update"
+        value={updateName}
+        onChange={setUpdateName}
+      />
+      <button onClick={e => submitNewName(e, factory._id)}>Update</button>
     </form>
-  )
-}
+  );
+};
 
 export default RenameForm;
